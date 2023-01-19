0dada9a72c6525549a00aee010777c9b97f0200f
edKotinsky <edkotinsky@gmail.com>
2023-01-19 10:29:47 +0600

    function avr_add_executable, add AVR_STRIP variable

--- avr-gcc.cmake
+++ avr-gcc.cmake
@@ -2,6 +2,12 @@ cmake_minimum_required(VERSION 3.0.0)
 
 set(CMAKE_EXPORT_COMPILE_COMMANDS ON)
 
+# avr-gcc: error: unrecognized command line option ‘-rdynamic’
+set(CMAKE_SHARED_LIBRARY_LINK_C_FLAGS "")
+set(CMAKE_SHARED_LIBRARY_LINK_CXX_FLAGS "")
+
+set(CMAKE_RUNTIME_OUTPUT_DIRECTORY "${CMAKE_CURRENT_BINARY_DIR}/bin")
+
 ##########################################################################
 # status messages
 ##########################################################################
@@ -21,6 +27,7 @@ find_program(AVR_CXX avr-g++ REQUIRED)
 find_program(AVR_OBJCOPY avr-objcopy REQUIRED)
 find_program(AVR_SIZE_TOOL avr-size REQUIRED)
 find_program(AVR_OBJDUMP avr-objdump REQUIRED)
+find_program(AVR_STRIP avr-strip REQUIRED)
 
 ##########################################################################
 # toolchain starts with defining mandatory variables
@@ -101,155 +108,183 @@ endif()
 
 message(STATUS "Build type: ${CMAKE_BUILD_TYPE}")
 
-# avr-gcc: error: unrecognized command line option ‘-rdynamic’
-set(CMAKE_SHARED_LIBRARY_LINK_C_FLAGS "")
-set(CMAKE_SHARED_LIBRARY_LINK_CXX_FLAGS "")
-
-set(CMAKE_RUNTIME_OUTPUT_DIRECTORY "${CMAKE_CURRENT_BINARY_DIR}/bin")
-
 ##########################################################################
+# avr_add_executable
+# - IN_VAR: AVR_TARGET
 #
+# Creates targets and dependencies for AVR toolchain, building an executable.
+# Calls add_executable with AVR_TARGET.
 ##########################################################################
-add_executable(${AVR_TARGET} ${SOURCES})
-target_link_libraries(${AVR_TARGET} PRIVATE ${AVR_INCLUDE})
+function(avr_add_executable AVR_TARGET)
 
-set(ELF_FILE "${AVR_TARGET}_${AVR_MCU}.elf")
-set(HEX_FILE "${AVR_TARGET}_${AVR_MCU}.hex")
-set(EEP_FILE "${AVR_TARGET}_${AVR_MCU}.eep")
+    if(NOT ARGN)
+        message(FATAL_ERROR "No source files given for ${AVR_TARGET}")
+    endif()
 
-set_target_properties(${AVR_TARGET} PROPERTIES OUTPUT_NAME ${ELF_FILE})
+    set(FILENAME "${AVR_TARGET}-${AVR_MCU}")
+    set(ELF_FILE "${FILENAME}.elf")
+    set(HEX_FILE "${FILENAME}.hex")
+    set(EEP_FILE "${FILENAME}.eep")
+    set(MAP_FILE "${FILENAME}.map")
+    set(LST_FILE "${FILENAME}.lst")
 
-# strip
-add_custom_target(strip ALL
-    COMMAND avr-strip ${ELF_FILE}
-    COMMAND ${AVR_SIZE_TOOL} ${AVR_SIZE_ARGS} ${ELF_FILE}
-    DEPENDS ${AVR_TARGET}
-    WORKING_DIRECTORY ${CMAKE_RUNTIME_OUTPUT_DIRECTORY}
-    VERBATIM
-    USES_TERMINAL
-)
+    add_executable(${AVR_TARGET} EXCLUDE_FROM_ALL ${ARGN})
 
-# create hex file
-add_custom_target(hex ALL
-    ${AVR_OBJCOPY} -R .eeprom -O ihex ${ELF_FILE} ${HEX_FILE}
-    DEPENDS strip
-    WORKING_DIRECTORY ${CMAKE_RUNTIME_OUTPUT_DIRECTORY}
-    VERBATIM
-    USES_TERMINAL
-)
+    set_target_properties(${AVR_TARGET}
+        PROPERTIES
+        OUTPUT_NAME ${ELF_FILE}
+        LINK_FLAGS 
+        "-mmcu=${AVR_MCU} -Wl,--gc-sections -mrelax -Wl,-Map,${MAP_FILE}"
+    )
 
-# eeprom
-add_custom_target(eeprom ALL
-    ${AVR_OBJCOPY} -j .eeprom --set-section-flags=.eeprom=alloc,load
-    --change-section-lma .eeprom=0 -O ihex ${ELF_FILE} ${EEP_FILE}
-    DEPENDS strip
-    WORKING_DIRECTORY ${CMAKE_RUNTIME_OUTPUT_DIRECTORY}
-    VERBATIM
-    USES_TERMINAL
-)
+    # strip
+    add_custom_target(strip ALL
+        COMMAND ${AVR_STRIP} ${ELF_FILE}
+        COMMAND ${AVR_SIZE_TOOL} ${AVR_SIZE_ARGS} ${ELF_FILE}
+        DEPENDS ${AVR_TARGET}
+        WORKING_DIRECTORY ${CMAKE_RUNTIME_OUTPUT_DIRECTORY}
+        VERBATIM
+        USES_TERMINAL
+    )
 
-# upload - with avrdude
-add_custom_target(upload
-    ${AVR_UPLOADTOOL} 
-    -p ${AVR_MCU} 
-    -c ${AVR_PROGRAMMER} 
-    ${AVR_UPLOADTOOL_OPTIONS} 
-    -U flash:w:${HEX_FILE} 
-    -P ${AVR_UPLOADTOOL_PORT}
-    DEPENDS hex
-    COMMENT "Uploading ${hex_file} to ${AVR_MCU} using ${AVR_PROGRAMMER}"
-    WORKING_DIRECTORY ${CMAKE_RUNTIME_OUTPUT_DIRECTORY}
-    VERBATIM
-    USES_TERMINAL
-)
+    # create hex file
+    add_custom_target(hex ALL
+        ${AVR_OBJCOPY} -R .eeprom -O ihex ${ELF_FILE} ${HEX_FILE}
+        DEPENDS strip
+        WORKING_DIRECTORY ${CMAKE_RUNTIME_OUTPUT_DIRECTORY}
+        VERBATIM
+        USES_TERMINAL
+    )
 
-# upload eeprom only - with avrdude
-# see also bug http://savannah.nongnu.org/bugs/?40142
-add_custom_target(upload_eeprom
-    ${AVR_UPLOADTOOL} 
-    -p ${AVR_MCU} 
-    -c ${AVR_PROGRAMMER} 
-    ${AVR_UPLOADTOOL_OPTIONS} 
-    -U eeprom:w:${EEP_FILE} 
-    -P ${AVR_UPLOADTOOL_PORT}
-    DEPENDS eeprom
-    COMMENT "Uploading ${EEP_FILE} to ${AVR_MCU} using ${AVR_PROGRAMMER}"
-    WORKING_DIRECTORY ${CMAKE_RUNTIME_OUTPUT_DIRECTORY}
-    VERBATIM
-    USES_TERMINAL
-)
+    # eeprom
+    add_custom_target(eeprom ALL
+        ${AVR_OBJCOPY} -j .eeprom --set-section-flags=.eeprom=alloc,load
+        --change-section-lma .eeprom=0 -O ihex ${ELF_FILE} ${EEP_FILE}
+        DEPENDS strip
+        WORKING_DIRECTORY ${CMAKE_RUNTIME_OUTPUT_DIRECTORY}
+        VERBATIM
+        USES_TERMINAL
+    )
 
-# get status
-add_custom_target(get_status
-    ${AVR_UPLOADTOOL} 
-    -p ${AVR_MCU} 
-    -c ${AVR_PROGRAMMER} 
-    -P ${AVR_UPLOADTOOL_PORT} 
-    -n 
-    -v
-    COMMENT "Get status from ${AVR_MCU}"
-)
+    # disassemble
+    add_custom_command(
+        OUTPUT ${LST_FILE}
+        COMMAND ${AVR_OBJDUMP} -d ${ELF_FILE}
+        DEPENDS strip
+        WORKING_DIRECTORY ${CMAKE_RUNTIME_OUTPUT_DIRECTORY}
+        VERBATIM
+        USES_TERMINAL
+    )
 
-# get fuses
-add_custom_target(get_fuses
-    ${AVR_UPLOADTOOL} 
-    -p ${AVR_MCU} 
-    -c ${AVR_PROGRAMMER} 
-    -P ${AVR_UPLOADTOOL_PORT} 
-    -n
-    -U lfuse:r:-:b
-    -U hfuse:r:-:b
-    COMMENT "Get fuses from ${AVR_MCU}"
-)
+    # clean
+    get_directory_property(clean_files ADDITIONAL_MAKE_CLEAN_FILES)
+    set_directory_properties(PROPERTIES 
+        ADDITIONAL_MAKE_CLEAN_FILES ${MAP_FILE})
 
-# set fuses
-add_custom_target(set_fuses
-    ${AVR_UPLOADTOOL} 
-    -p ${AVR_MCU} 
-    -c ${AVR_PROGRAMMER} 
-    -P ${AVR_UPLOADTOOL_PORT}
-    -U lfuse:w:${AVR_L_FUSE}:m
-    -U hfuse:w:${AVR_H_FUSE}:m
-    COMMENT "Setup: High Fuse: ${AVR_H_FUSE} Low Fuse: ${AVR_L_FUSE}"
-)
+    # upload - with avrdude
+    add_custom_target(upload
+        ${AVR_UPLOADTOOL} 
+        -p ${AVR_MCU} 
+        -c ${AVR_PROGRAMMER} 
+        ${AVR_UPLOADTOOL_OPTIONS} 
+        -U flash:w:${HEX_FILE} 
+        -P ${AVR_UPLOADTOOL_PORT}
+        DEPENDS hex
+        COMMENT "Uploading ${hex_file} to ${AVR_MCU} using ${AVR_PROGRAMMER}"
+        WORKING_DIRECTORY ${CMAKE_RUNTIME_OUTPUT_DIRECTORY}
+        VERBATIM
+        USES_TERMINAL
+    )
 
-# get oscillator calibration
-add_custom_target(get_calibration
-    ${AVR_UPLOADTOOL} 
-    -p ${AVR_MCU} 
-    -c ${AVR_PROGRAMMER} 
-    -P ${AVR_UPLOADTOOL_PORT}
-    -U calibration:r:${AVR_MCU}_calib.tmp:r
-    COMMENT "Write calibration status of internal oscillator 
-to ${AVR_MCU}_calib.tmp."
-    WORKING_DIRECTORY ${CMAKE_RUNTIME_OUTPUT_DIRECTORY}
-    VERBATIM
-    USES_TERMINAL
-)
+    # upload eeprom only - with avrdude
+    # see also bug http://savannah.nongnu.org/bugs/?40142
+    add_custom_target(upload_eeprom
+        ${AVR_UPLOADTOOL} 
+        -p ${AVR_MCU} 
+        -c ${AVR_PROGRAMMER} 
+        ${AVR_UPLOADTOOL_OPTIONS} 
+        -U eeprom:w:${EEP_FILE} 
+        -P ${AVR_UPLOADTOOL_PORT}
+        DEPENDS eeprom
+        COMMENT "Uploading ${EEP_FILE} to ${AVR_MCU} using ${AVR_PROGRAMMER}"
+        WORKING_DIRECTORY ${CMAKE_RUNTIME_OUTPUT_DIRECTORY}
+        VERBATIM
+        USES_TERMINAL
+    )
 
-# set oscillator calibration
-add_custom_target(set_calibration
-    ${AVR_UPLOADTOOL} 
-    -p ${AVR_MCU} 
-    -c ${AVR_PROGRAMMER} 
-    -P ${AVR_UPLOADTOOL_PORT}
-    -U calibration:w:${AVR_MCU}_calib.hex
-    COMMENT "Program calibration status of internal oscillator 
-from ${AVR_MCU}_calib.hex."
-    WORKING_DIRECTORY ${CMAKE_RUNTIME_OUTPUT_DIRECTORY}
-    VERBATIM
-    USES_TERMINAL
-)
+    # get status
+    add_custom_target(get_status
+        ${AVR_UPLOADTOOL} 
+        -p ${AVR_MCU} 
+        -c ${AVR_PROGRAMMER} 
+        -P ${AVR_UPLOADTOOL_PORT} 
+        -n 
+        -v
+        COMMENT "Get status from ${AVR_MCU}"
+    )
 
-# disassemble
-# this target will work only on *nix systems due to the shell capabilities
-add_custom_target(disassemble
-    ${AVR_OBJDUMP} 
-    -h 
-    -S ${ELF_FILE} > "disasm_${AVR_MCU}.lst"
-    DEPENDS strip
-    COMMENT "Disassemble ${ELF_FILE} >>> disasm_${AVR_MCU}.lst"
-    WORKING_DIRECTORY ${CMAKE_RUNTIME_OUTPUT_DIRECTORY}
-    VERBATIM
-    USES_TERMINAL
-)
+    # get fuses
+    add_custom_target(get_fuses
+        ${AVR_UPLOADTOOL} 
+        -p ${AVR_MCU} 
+        -c ${AVR_PROGRAMMER} 
+        -P ${AVR_UPLOADTOOL_PORT} 
+        -n
+        -U lfuse:r:-:b
+        -U hfuse:r:-:b
+        COMMENT "Get fuses from ${AVR_MCU}"
+    )
+
+    # set fuses
+    add_custom_target(set_fuses
+        ${AVR_UPLOADTOOL} 
+        -p ${AVR_MCU} 
+        -c ${AVR_PROGRAMMER} 
+        -P ${AVR_UPLOADTOOL_PORT}
+        -U lfuse:w:${AVR_L_FUSE}:m
+        -U hfuse:w:${AVR_H_FUSE}:m
+        COMMENT "Setup: High Fuse: ${AVR_H_FUSE} Low Fuse: ${AVR_L_FUSE}"
+    )
+
+    # get oscillator calibration
+    add_custom_target(get_calibration
+        ${AVR_UPLOADTOOL} 
+        -p ${AVR_MCU} 
+        -c ${AVR_PROGRAMMER} 
+        -P ${AVR_UPLOADTOOL_PORT}
+        -U calibration:r:${AVR_MCU}_calib.tmp:r
+        COMMENT "Write calibration status of internal oscillator 
+    to ${AVR_MCU}_calib.tmp."
+        WORKING_DIRECTORY ${CMAKE_RUNTIME_OUTPUT_DIRECTORY}
+        VERBATIM
+        USES_TERMINAL
+    )
+
+    # set oscillator calibration
+    add_custom_target(set_calibration
+        ${AVR_UPLOADTOOL} 
+        -p ${AVR_MCU} 
+        -c ${AVR_PROGRAMMER} 
+        -P ${AVR_UPLOADTOOL_PORT}
+        -U calibration:w:${AVR_MCU}_calib.hex
+        COMMENT "Program calibration status of internal oscillator 
+        from ${AVR_MCU}_calib.hex."
+        WORKING_DIRECTORY ${CMAKE_RUNTIME_OUTPUT_DIRECTORY}
+        VERBATIM
+        USES_TERMINAL
+    )
+
+    # disassemble
+    add_custom_target(disassemble
+        ${AVR_OBJDUMP} 
+        -h 
+        -S ${ELF_FILE} > "disasm_${AVR_MCU}.lst"
+        DEPENDS strip
+        COMMENT "Disassemble ${ELF_FILE} >>> disasm_${AVR_MCU}.lst"
+        WORKING_DIRECTORY ${CMAKE_RUNTIME_OUTPUT_DIRECTORY}
+        VERBATIM
+        USES_TERMINAL
+    )
+
+endfunction()
+
